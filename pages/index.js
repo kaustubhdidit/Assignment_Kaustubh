d5b9e82f3683ec7a4d6b2fd1f8afab8cdfcd282d
Kaustubh Dixit <92023578+kaustubhdidit@users.noreply.github.com>
2024-04-02 11:38:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from "next/image";
 
 import bell from "../public/images/bell1.png"
@@ -26,32 +26,40 @@ export default function Home() {
   };
 
   const sendNotification = () => {
-    if (notificationGranted) {
-      new Notification('Hello!', {
-        body: 'This is a notification from your Next.js app!',
+    if ('serviceWorker' in navigator && 'ServiceWorkerRegistration' in window) {
+      navigator.serviceWorker.getRegistration().then(function(registration) {
+        if (registration) {
+          registration.showNotification('Hello!', {
+            body: 'This is a notification from your Next.js app!'
+          });
+        } else {
+          alert('Service worker not registered. Notifications cannot be sent.');
+        }
+      }).catch(function(err) {
+        console.error('Error retrieving service worker registration:', err);
       });
     } else {
-      alert('Please grant permission to receive notifications.');
+      alert('Service workers are not supported on this browser.');
     }
   };
 
   return (
-    <div className="holder" style={{ display:"flex", justifyContent:"center"}}>
-      <div className="layout" style={{ justifyContent:"center"}}>
-        <div className="head" style={{ display: "flex", flexDirection: "column", alignItems: "center"}}>
+    <div className="holder" style={{ display: "flex", justifyContent: "center" }}>
+      <div className="layout" style={{ justifyContent: "center" }}>
+        <div className="head" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
           <h1>Lorem Ipsum...</h1>
           <h2>Lorem ipsum dolor sit amet.</h2>
         </div>
         <div className="image" style={{ display: 'flex', justifyContent: 'center' }}>
-          <div className="c" id="c0" style={{ display: 'flex', justifyContent: 'center', margin:"auto" }} >
-            <div className="cin" id="c0in" style={{ display: 'flex', justifyContent: 'center', margin:"auto" }} >
-              <div className="c" id="c1" style={{ display: 'flex', justifyContent: 'center', margin:"auto" }} >
-                <div className="cin" id="c1in" style={{ display: 'flex', justifyContent: 'center', margin:"auto" }} >
-                  <div className="c" id="c2" style={{ display: 'flex', justifyContent: 'center',margin:"auto" }} >
-                    <div className="cin" id="c2in" style={{ display: 'flex', justifyContent: 'center', margin:"auto" }} >
-                      <div className="c" id="c3" style={{ display: 'flex', justifyContent: 'center', margin:"auto" }} >
-                        <div className="cin" id="c3in" style={{ display: 'flex', justifyContent: 'center', margin:"auto" }} >
-                          <div id="one" style={{margin:"auto"}}>
+          <div className="c" id="c0" style={{ display: 'flex', justifyContent: 'center', margin: "auto" }} >
+            <div className="cin" id="c0in" style={{ display: 'flex', justifyContent: 'center', margin: "auto" }} >
+              <div className="c" id="c1" style={{ display: 'flex', justifyContent: 'center', margin: "auto" }} >
+                <div className="cin" id="c1in" style={{ display: 'flex', justifyContent: 'center', margin: "auto" }} >
+                  <div className="c" id="c2" style={{ display: 'flex', justifyContent: 'center', margin: "auto" }} >
+                    <div className="cin" id="c2in" style={{ display: 'flex', justifyContent: 'center', margin: "auto" }} >
+                      <div className="c" id="c3" style={{ display: 'flex', justifyContent: 'center', margin: "auto" }} >
+                        <div className="cin" id="c3in" style={{ display: 'flex', justifyContent: 'center', margin: "auto" }} >
+                          <div id="one" style={{ margin: "auto" }}>
                             <Image src={bell} alt="hello" width={120} height={130} />
                           </div>
                         </div>
